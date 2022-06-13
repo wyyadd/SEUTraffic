5a2153274ba6129ee9196a60d0fc2dd945d2179c
wyyadd <wyyadd@gmail.com>
2022-06-13 12:04:52 +0800

    DSA、DOCP算法60%完成

--- src/algo/DSA/DSA_Agent.cpp
+++ src/algo/DSA/DSA_Agent.cpp
@@ -5,8 +5,24 @@
 #include "DSA_Agent.h"
 
 namespace ALGO {
-    void DSA_Agent::sendMessage(Message message, DSA_Agent *neighbour) {
-        neighbour->getMailBox().push_back(message);
+    void DSA_Agent::sendMessage() {
+        for (int movementPhase = 0; movementPhase < 4; ++movementPhase) {
+            // agent's movementPhase on myself
+            auto phase = static_cast<MovementPhases>(movementPhase);
+            double cost = 0;
+            // add local cost
+            cost += costGraph[phase][4].cost.empty() ? 0 : costGraph[phase][4].cost[0].second;
+            // add message from neighbour
+            cost += receivedMessage[phase].Q;
+            // send cost to neighbour
+            for (int i = 0; i < 4; ++i) {
+                auto neighbour_phase = static_cast<MovementPhases>(i);
+                for (auto &neighbour_cost_pair: costGraph[phase][neighbour_phase].cost) {
+                    // TODO: add lock
+                    neighbour_cost_pair.first->getMailBox()[neighbour_phase].Q += neighbour_cost_pair.second + cost;
+                }
+            }
+        }
     }
 
     void DSA_Agent::updateInAgents(DSA_Agent *agent) {
@@ -31,43 +47,86 @@ namespace ALGO {
             outAgents[East] = agent;
     }
 
-    double DSA_Agent::getCost(MovementPhases movementPhase) {
-        double cost = 0;
+    void DSA_Agent::generateCostGraph(MovementPhases movementPhase) {
         switch (movementPhase) {
             case WE_Straight:
-            case SN_Left:
-            {
-                cost += getCost(West, movementPhase);
-                cost += getCost(East, movementPhase);
+            case WE_Left: {
+                generateCost(West, movementPhase);
+                generateCost(East, movementPhase);
                 break;
             }
-            case WE_Left:
             case SN_Straight:
-            {
-                cost += getCost(North, movementPhase);
-                cost += getCost(South, movementPhase);
+            case SN_Left: {
+                generateCost(North, movementPhase);
+                generateCost(South, movementPhase);
                 break;
             }
         }
-        return cost;
     }
 
-    double DSA_Agent::getCost(Direction direction, MovementPhases movementPhase) {
+    void DSA_Agent::generateCost(Direction direction, MovementPhases movementPhase) {
         if (outAgents[direction] == nullptr && !neighbours[direction]->isVirtualIntersection())
-            return 0;
-        auto neighbour = outAgents[direction] == nullptr ? neighbours[direction] : outAgents[direction];
-        return getCost(movementPhase, neighbour);
+            return;
+        if (outAgents[direction] == nullptr)
+            return generateLocalCost(neighbours[direction], movementPhase);
+        else
+            return generateCostWithNeighbour(outAgents[direction], movementPhase);
     }
 
-    double DSA_Agent::getCost(MovementPhases movementPhase, Intersection *neighbour) {
+    void DSA_Agent::generateCostWithNeighbour(DSA_Agent *neighbour, MovementPhases movementPhase) {
+        // this agent set movementPhase
+        // neighbour set four movement
+        for (int phase = 0; phase < 4; ++phase) {
+            double cost = 0;
+            auto neighbour_phase = static_cast<MovementPhases>(movementPhase);
+            // TODO: intersection set phase
+            // TODO: engine predict
+            // neighbour set neighbour_phase
+            // engine predict
+            for (auto &roadLink: roadLinks) {
+                if (roadLink.getStartRoad()->getStartIntersection()->getId() == neighbour->getId()
+                    && (int) roadLink.getRoadLinkType() % 2 == (int) movementPhase) {
+                    cost += std::pow(roadLink.getVehicleCnt(), 2);
+                }
+            }
+            for (auto &roadLink: neighbour->getRoadLinks()) {
+                if (roadLink.getStartRoad()->getStartIntersection()->getId() == getId() &&
+                    (int) roadLink.getRoadLinkType() % 2 == (int) movementPhase) {
+                    cost += std::pow(roadLink.getVehicleCnt(), 2);
+                }
+            }
+            costGraph[movementPhase][neighbour_phase].cost.emplace_back(neighbour, cost);
+            // engine predict done
+        }
+    }
+
+    void DSA_Agent::generateLocalCost(Intersection *neighbour, MovementPhases movementPhase) {
         double cost = 0;
+        // this set phase
+        // engine predict
         for (auto &roadLink: roadLinks) {
             if (roadLink.getStartRoad()->getStartIntersection() == neighbour
                 && (int) roadLink.getRoadLinkType() % 2 == (int) movementPhase) {
-                cost += (double)(roadLink.getVehicleCnt() * roadLink.getVehicleCnt());
+                // TODO: 修改计算方法
+                cost += std::pow(roadLink.getVehicleCnt(), 2);
             }
         }
-        return cost;
+        // engine predict done
+        costGraph[movementPhase][4].cost.emplace_back(nullptr, cost);
+    }
+
+    void DSA_Agent::run() {
+        // wait receive all message
+        generateCostGraph();
+        sendMessage();
+        // reverse generateCostGraph and sendMessage
+        // make decision
+    }
+
+    void DSA_Agent::generateCostGraph() {
+        for (int movementPhase = 0; movementPhase < 4; ++movementPhase) {
+            generateCostGraph(static_cast<MovementPhases>(movementPhase));
+        }
     }
 
 
--- src/algo/DSA/DSA_Agent.h
+++ src/algo/DSA/DSA_Agent.h
@@ -9,47 +9,64 @@
 #include <unordered_map>
 #include <deque>
 #include <vector>
+#include <cmath>
 
 namespace ALGO {
     using SEUTraffic::Intersection;
     using std::string;
     using std::pair;
 
-    struct Message {
-        int agentId;
-        int message;
-    };
-
-    enum MovementPhases {
-        WE_Straight = 0, WE_Left = 1, SN_Straight = 2, SN_Left = 3
-    };
-
     class DSA_Agent : public Intersection {
     private:
+        struct Message {
+            double Q = 0;
+        };
+
+        struct Cost {
+            std::vector<std::pair<DSA_Agent*, double>> cost;
+        };
+
+        enum MovementPhases {
+            WE_Straight = 0, WE_Left = 1, SN_Straight = 2, SN_Left = 3
+        };
         int agentId;
-        std::deque<Message> receivedMessage;
+        std::vector<Message> receivedMessage;
         std::vector<DSA_Agent *> inAgents;
         std::vector<DSA_Agent *> outAgents;
+        // 4 rows represent 4 movementPhases, 5 column: neighbour's four movementPhase, local cost
+        // 4行代表我们的四个movementPhases, 5列:前四列表示neighbour采取的4个movementPhase下产生的cost, 第5列表示localCost
+        std::vector<std::vector<Cost>> costGraph;
     private:
-        double getCost(Direction direction, MovementPhases movementPhase);
-        double getCost(MovementPhases movementPhase, Intersection* neighbour);
+        void generateCostGraph(MovementPhases movementPhase);
+
+        // direction = neighbour's direction
+        void generateCost(Direction direction, MovementPhases movementPhase);
+
+        void generateCostWithNeighbour(DSA_Agent *neighbour, MovementPhases movementPhase);
+
+        void generateLocalCost(Intersection *neighbour, MovementPhases movementPhase);
+
     public:
         DSA_Agent(int id, Intersection *intersection) : Intersection(*intersection), agentId(id) {
             inAgents.resize(4, nullptr);
             outAgents.resize(4, nullptr);
+            receivedMessage.resize(4, Message());
+            costGraph.resize(4, std::vector<Cost>(5, Cost()));
         }
 
         int getAgentId() const { return agentId; }
 
-        std::deque<Message> &getMailBox() { return receivedMessage; }
+        std::vector<Message> &getMailBox() { return receivedMessage; }
 
-        static void sendMessage(Message message, DSA_Agent *neighbour);
+        void sendMessage();
 
         void updateInAgents(DSA_Agent *agent);
 
         void updateOutAgents(DSA_Agent *agent);
 
-        double getCost(MovementPhases movementPhase);
+        void generateCostGraph();
+
+        void run();
     };
 } // ALGO
 
